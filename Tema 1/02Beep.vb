8373c3b372ca2db5424cf0db96a525d94868fd87
RamonRS <celmon1@gmail.com>
2018-09-19 11:32:06 +0200

    Ejemplos tema 1

---
+++ Tema 1/02Beep.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hola Mundo!!!!")
+        Console.WriteLine()
+        Console.ForegroundColor = ConsoleColor.DarkYellow
+        Console.WriteLine("Bienvenidos al maravilloso mundo de la programación")
+        'Para evitar que se cierre la consola
+
+        Console.ForegroundColor = ConsoleColor.DarkRed
+        Console.Title = "Mi primer programa"
+        Console.Beep(2000, 4000)
+        Console.WriteLine("Me gusta programar")
+
+        Console.ReadLine()
+    End Sub
+
+End Module
