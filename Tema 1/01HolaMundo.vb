1861c5fecf2ae2ae377453dedae6c0c03d16eec1
RamonRS <celmon1@gmail.com>
2018-09-19 10:10:28 +0200

    Ejemplo dia 1

---
+++ Tema 1/01HolaMundo.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hola Mundo!!!!")
+        Console.WriteLine()
+        Console.WriteLine("Bienvenidos al maravilloso mundo de la programación")
+        'Para evitar que se cierre la consola
+
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
