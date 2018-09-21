01604976d61f802c65027da8bce41c69e8c9b38f
RamonRS <celmon1@gmail.com>
2018-09-21 06:31:54 +0200

    Ejemplos tema2

--- Tema 2/Ejemplos/04Conversiones.vb
+++ Tema 2/Ejemplos/04Conversiones.vb
@@ -7,6 +7,10 @@ Module Conversiones
         Dim nShort As Short = n
 
         'Conversiones explicitas: Guardar un tipo de variable en una varaible de menor tamaño
+        'Clase Convert
+        'Convert.toByte() , Convert.toInt16() , Convert.toInt32()
+        'Convert.toInt64() , Convert.toSingle() , Convert.toDouble()
+
         Dim n2 As Integer = 10
         Dim n3 As Short = Convert.ToInt16(n2)
 
@@ -17,6 +21,19 @@ Module Conversiones
         'Problema pérdida de precision
         Dim temperatura As Single = 17.8
         Dim t As Integer = Convert.ToInt32(temperatura)
+
+        'Variables double y single
+        Dim dato As Double = 34.67
+        Dim dato2 As Single = Convert.ToSingle(dato)
+
+        'Situaciones menos evidentes
+        Dim numero As Byte = 5
+        numero = Convert.ToByte(numero + 1)
+
+
+
+
+
         Console.WriteLine(t)
 
         Console.ReadLine()
