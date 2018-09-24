e1fce85216cd835ea7483a6035030a47708addb3
RamonRS <celmon1@gmail.com>
2018-09-24 11:59:46 +0200

    Boletin

---
+++ Tema 2/Boletin/Ejercicio04.vb
@@ -0,0 +1,27 @@
+﻿Option Strict On
+Module Ejercicio04
+
+    Sub Main()
+        Dim radio, area, longitud, volumen As Double
+        Const PI As Double = 3.141516
+        radio = 5
+
+        longitud = 2 * Math.PI * radio
+
+        area = Math.PI * Math.Pow(radio, 2)
+        area = Math.Round(area, 2)
+
+        volumen = Math.Round((4 / 3) * Math.PI * Math.Pow(radio, 3), 2)
+
+        Console.WriteLine("Longitud : " & Math.Round(longitud, 2) & vbNewLine & "Area : " & area)
+        Console.WriteLine("Volumen : " & volumen)
+
+        Console.WriteLine(longitud)
+
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
---
+++ Tema 2/Boletin/Ejercicio05.vb
@@ -0,0 +1,22 @@
+﻿Option Strict On
+
+Module Ejercicio05
+
+    Sub Main()
+        Dim precioUnidad As Double = 5.75
+        Dim cantidad As Short = 1000
+        Dim precioFinal As Single = Convert.ToSingle(precioUnidad * cantidad)
+        Dim descuento As Byte = 15
+        Dim precioConDescuento As Single
+
+        Console.WriteLine("Precio final : " & precioFinal)
+
+        ' Descuento --> precioConDescuento = (precioFinal * 15) / 100
+        ' Precio con descuento --> precioConDescuento = precioFinal - Convert.ToSingle(precioFinal * 0.15)
+
+        precioConDescuento = Convert.ToSingle(precioFinal * 0.85)
+        Console.WriteLine("Aplicando un " & descuento & " % " & precioConDescuento)
+        Console.ReadLine()
+
+    End Sub
+End Module
