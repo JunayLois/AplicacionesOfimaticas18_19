1eff2af0462ff6590057b9d0f2f8d84a54068905
RamonRS <celmon1@gmail.com>
2018-10-04 13:29:49 +0200

    Boletin

---
+++ Tema 3/boletin/Ejercicio07.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        'Ejercicio
+        Dim n1 As Double = 4
+        Dim n2 As Double = 2
+        Dim suma As Double = n1 + n2
+
+        Dim positivo As Boolean = suma > 0
+
+        If positivo Then
+            Console.WriteLine("Raiz de " & suma & " : " & Math.Sqrt(suma))
+        Else
+            Console.WriteLine("No existe raiz de número negativo")
+        End If
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
