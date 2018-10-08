9c6abc12bf6a7fec0bedbe7c656ed8fa7698f29f
RamonRS <celmon1@gmail.com>
2018-10-08 12:51:44 +0200

    Boletin 3

---
+++ Tema 3/boletin/Ejercicio11.vb
@@ -0,0 +1,35 @@
+﻿
+Module Ejercicio11
+    Sub Main()
+        Dim numero As Integer = 7
+
+        If numero Mod 2 = 0 Or numero Mod 3 = 0 Then
+            Console.WriteLine("Múltiplo de 2 o de 3")
+        Else
+            Console.WriteLine("No es múltiplo de 2 o 3")
+        End If
+
+        'Detectar si es múltiplo de 2 o de 3 o de ninguno o de los 2
+
+        If numero Mod 2 = 0 Then
+            If numero Mod 3 = 0 Then
+                Console.WriteLine("Múltiplo de 2 y de 3")
+            Else
+                Console.WriteLine("Múltiplo de 2 no de 3")
+            End If
+        Else
+            If numero Mod 3 = 0 Then
+                Console.WriteLine("Múltiplo de 3 y no de 2")
+            Else
+                Console.WriteLine("Ni de 2 ni de 3")
+            End If
+
+
+        End If
+
+        Console.ReadLine()
+
+
+
+    End Sub
+End Module
---
+++ Tema 3/boletin/Ejercicio12.vb
@@ -0,0 +1,16 @@
+﻿Module Ejercicio12
+    Sub Main()
+        Dim precio, precioDescuento As Double
+        precio = 120
+
+        If precio < 100 Then
+            'precio = precio - 0.1 * precio
+            precioDescuento = 0.9 * precio
+        Else
+            precioDescuento = 0.85 * precio
+        End If
+
+        Console.WriteLine("Precio final " & precioDescuento)
+
+    End Sub
+End Module
