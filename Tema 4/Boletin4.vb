22b6fdb5cc0324aea12c71a2914e6a762741b3d3
RamonRS <celmon1@gmail.com>
2018-10-16 14:30:14 +0200

    Boletin

--- Tema 4/Boletin4.vb
+++ Tema 4/Boletin4.vb
@@ -12,9 +12,11 @@ Module Module1
             Console.WriteLine("Escoge el ejercicio : ")
             Console.WriteLine("5. Evaluar nota")
             Console.WriteLine("6. Múltiplo de 4")
+            Console.WriteLine("8. Calculadora.")
             Console.WriteLine("9. Calcular gasto de agua")
             Console.WriteLine("10. Calcular salario neto:")
             Console.WriteLine("12. Escoger color")
+            Console.WriteLine("13. Resolver ecuación 2do grado")
             opcion = Convert.ToInt32(Console.ReadLine())
 
             Select Case opcion
@@ -64,6 +66,25 @@ Module Module1
                     Else
                         Console.WriteLine("Vapor")
                     End If
+                Case 8
+                    Console.WriteLine("Introduzca una opción:")
+                    Console.WriteLine("a. Sumar")
+                    Console.WriteLine("b. Restar")
+                    Console.WriteLine("c. Multiplicar")
+                    Dim opcion2 As Char = Convert.ToChar(Console.ReadLine())
+
+                    Select Case opcion2
+                        Case "a"c
+                            Dim n1, n2 As Integer
+                            Console.WriteLine("Introduce n1")
+                            n1 = Convert.ToInt32(Console.ReadLine())
+                            Console.WriteLine("Introduce n2")
+                            n2 = Convert.ToInt32(Console.ReadLine())
+                            Dim suma As Integer = n1 + n2
+                            Console.WriteLine("La suma es : " & suma)
+
+                    End Select
+
                 Case 9
                     Dim litros, pago As Double
                     Console.WriteLine("Introduce los litros.")
@@ -77,13 +98,12 @@ Module Module1
                         pago = 150 * 10 + (litros - 200) * 20
                     End If
 
+
                     If pago < 500 Then
                         pago = 500
                     End If
 
                     Console.WriteLine("Tiene que pagar : " & pago & " euros.")
-
-
 
                 Case 10
                     Dim horas, tasa, tasaExtra, salarioBruto, salarioNeto As Double
@@ -136,6 +156,59 @@ Module Module1
                             Console.WriteLine("Has escogido el negro")
 
                     End Select
+
+                Case 13
+                    'ax2 + bx + c = 0
+                    Dim a, b, c As Integer
+                    Dim sol1, sol2 As Double
+                    Dim solucion As Boolean = True
+                    a = 0
+                    b = 0
+                    c = 1
+
+                    'Si a = 0 --> bx + c = 0 --> sol1 = -c/b
+                    If a = 0 Then
+                        If b = 0 Then
+                            solucion = False
+                            Console.WriteLine("No se puede resolver")
+                        Else
+                            sol1 = -c / b
+                            sol2 = sol1
+                        End If
+                    ElseIf b = 0 Then
+                        'Si b = 0 --> ax2 + c = 0 --> sol = +- Raiz(-c/a)
+                        Dim radicando As Double = -c / a
+                        If radicando > 0 Then
+                            sol1 = Math.Sqrt(-c / a)
+                            sol2 = -Math.Sqrt(-c / a)
+                        Else
+                            solucion = False
+                            Console.WriteLine("No tiene solución real")
+                        End If
+
+                    ElseIf c = 0 Then
+                        'Si c = 0 --> ax2 + bx = 0 --> sol1 = 0 , sol2 = -b/a
+                        sol1 = 0
+                        sol2 = -b / a
+                    Else
+                        'sol1 = -b +-Raiz(b2 -4ac)/2a
+                        Dim radicando As Double = b * b - 4 * a * c
+
+                        If radicando >= 0 Then
+                            sol1 = (-b + Math.Sqrt(radicando)) / 2 * a
+                            sol2 = (-b - Math.Sqrt(radicando)) / 2 * a
+                        Else
+                            solucion = False
+                            Console.WriteLine("No tiene solución real")
+                        End If
+
+                    End If
+
+                    If solucion Then
+                        Console.WriteLine("Las soluciones son : ")
+                        Console.WriteLine("x1 = " & sol1)
+                        Console.WriteLine("x2 = " & sol2)
+                    End If
 
 
                 Case Else
