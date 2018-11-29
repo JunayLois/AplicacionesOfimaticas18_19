44a343abd8e0a02e5010da12ddaee89621b2d541
RamonRS <celmon1@gmail.com>
2018-11-29 11:45:31 +0100

    Boletin 6

--- Tema 6 - Arrays/boletin6.vb
+++ Tema 6 - Arrays/boletin6.vb
@@ -169,8 +169,221 @@
                     For Each valor As Integer In m2
                         Console.Write(valor & " ")
                     Next
+
+                Case 8
+                    Dim x As Integer = 5
+
+                    'Crear un array de x elementos
+                    Dim matriz(x - 1) As Integer
+
+                    For i As Integer = 0 To matriz.Length - 1 Step 1
+                        Console.WriteLine("Introduce valor posicion " & i)
+                        matriz(i) = Convert.ToInt32(Console.ReadLine())
+                    Next
+                    Dim posicion As Integer
+                    Console.WriteLine("Escoja una posición : ")
+                    posicion = Convert.ToInt32(Console.ReadLine())
+
+                    'Intercambiar el último con el valor de la posición escogida
+                    If posicion < 0 Or posicion > matriz.Length - 1 Then
+                        Console.WriteLine("Error en la posición")
+                    Else
+                        Dim aux As Integer = matriz(matriz.Length - 1)
+                        matriz(matriz.Length - 1) = matriz(posicion)
+                        matriz(posicion) = aux
+                    End If
+                    Console.WriteLine("Array modificado: ")
+                    'Mostrar el array modificado
+                    For Each valor As Integer In matriz
+                        Console.Write(valor & " ")
+                    Next
+                    Console.WriteLine()
+
+                Case 9
+                    Dim matriz() As Integer = {6, -3, 9, 0, 7, 1}
+                    Dim posicion As Integer = -1
+                    For i As Integer = 0 To matriz.Length - 1 Step 1
+                        If matriz(i) < 0 Then
+                            posicion = i
+                            Console.Write("El primer negativo es " & matriz(i) & " en la posición " & i)
+                            Exit For
+                        End If
+                    Next
+                    If posicion = -1 Then
+                        Console.WriteLine("No hay números negativos")
+                    End If
+
+                Case 10
+                    Dim matriz() As Integer = {5, 1, 3, 5, 8, 1, 8}
+                    Dim valorBuscado As Integer
+                    Console.WriteLine("Intro valor que desea buscar")
+                    valorBuscado = Convert.ToInt32(Console.ReadLine())
+                    Dim encontrado As Boolean = False
+                    For i As Integer = 0 To matriz.Length - 1 Step 1
+                        If matriz(i) = valorBuscado Then
+                            encontrado = True
+                            Console.WriteLine("Posición " & i)
+                        End If
+                    Next
+
+                    If Not encontrado Then
+                        Console.WriteLine("No está en la matriz")
+                    End If
+
+                Case 11
+                    Dim matriz1() As Integer = {3, 5, 1, 3, 7, 9}
+                    Dim matriz2(matriz1.Length - 1) As Integer
+
+                    Array.Copy(matriz1, matriz2, matriz1.Length)
+
+                    Array.Sort(matriz1)
+
+                    'Mostrar las dos matriz
+                    Console.WriteLine("Matriz 1")
+                    For Each p As Integer In matriz1
+                        Console.Write(p & " ")
+                    Next
+
+                    Console.WriteLine()
+                    Console.WriteLine("Matriz 2")
+
+                    For Each p As Integer In matriz2
+                        Console.Write(p & " ")
+                    Next
+                    Console.WriteLine()
+                    Dim valor As Integer = 7
+
+                    'Buscar en la primera matriz
+                    Dim posicion1 As Integer = Array.BinarySearch(matriz1, valor)
+                    If posicion1 < 0 Then
+                        Console.WriteLine(valor & " no está en la matriz")
+                    Else
+                        Console.WriteLine(valor & " está en la posición : " & posicion1)
+                    End If
+
+                    'Buscar en la segunda matriz
+                    Dim posicion2 As Integer = -1
+
+                    For i As Integer = 0 To matriz2.Length - 1 Step 1
+                        If matriz2(i) = valor Then
+                            posicion2 = i
+                            Exit For
+                        End If
+                    Next
+
+                    If posicion2 < 0 Then
+                        Console.WriteLine("No está en la matriz")
+                    Else
+                        Console.WriteLine(valor & " está en la posición " & posicion2)
+                    End If
+
+                Case 12
+                    Dim matriz() As Integer = {9, 0, 11, 6, -5, 4, 3, 2, 1}
+                    '0,9,6,11,-5
+                    For j As Integer = 0 To matriz.Length - 1 Step 1
+                        For i As Integer = 0 To matriz.Length - 2 Step 1
+                            If matriz(i) > matriz(i + 1) Then
+
+                                Dim auz As Integer = matriz(i)
+                                matriz(i) = matriz(i + 1)
+                                matriz(i + 1) = auz
+
+                            End If
+
+                        Next
+                    Next
+                    'Mostrar la matriz
+                    Console.WriteLine("Matriz ")
+                    For Each valor As Integer In matriz
+                        Console.Write(valor & " ")
+                    Next
+                    Console.WriteLine()
+
+                Case 13
+                    Dim matriz() As Integer = {5, 6, 1, 3, 7, 9}
+                    Dim maximo As Integer = matriz(0)
+                    Dim minimo As Integer = matriz(0)
+                    Dim posMax, posMin As Integer
+
+                    For i As Integer = 1 To matriz.Length - 1 Step 1
+                        If maximo < matriz(i) Then
+                            maximo = matriz(i)
+                            posMax = i + 1
+                        End If
+                    Next
+
+                    For i As Integer = 1 To matriz.Length - 1 Step 1
+                        If matriz(i) < minimo Then
+                            minimo = matriz(i)
+                            posMin = i + 1
+                        End If
+                    Next
+                    'Mostrar los resultados
+                    Console.WriteLine(maximo & " posicion: " & posMax)
+                    Console.WriteLine(minimo & " posicion: " & posMin)
+
+                Case 14
+                    Dim matriz() As Integer = {13, 6, 17, 19, 5}
+                    Dim primo As Boolean = True
+                    Dim contadorPrimos As Integer = 0
+                    For i As Integer = 0 To matriz.Length - 1 Step 1
+                        primo = True
+                        For j As Integer = 2 To Convert.ToInt32(matriz(i) / 2) Step 1
+                            If matriz(i) Mod j = 0 Then
+                                primo = False
+                                Exit For
+                            End If
+                        Next
+
+                        If primo Then
+                            contadorPrimos += 1
+                            Console.Write(matriz(i) & " ")
+                        End If
+                    Next
+                    Console.WriteLine()
+                    Console.WriteLine("Hay " & contadorPrimos & " numeros primos")
+
+                Case 15
+
+
+                Case 16
+                    Dim matriz(10) As Integer
+                    Dim aleatorio As Random = New Random()
+
+                    For i As Integer = 0 To matriz.Length - 1 Step 1
+                        matriz(i) = aleatorio.Next(1, 11)
+                    Next
+                    Dim encontrado As Boolean = 0
+                    'Mostrar la matriz
+                    For j As Integer = 0 To matriz.Length - 1 Step 1
+                        'Realizar una búsqueda en la matriz
+                        encontrado = False
+                        Dim contador As Integer = 0
+                        For i As Integer = 0 To matriz.Length - 1 Step 1
+                            If i <> j Then
+                                If matriz(i) = matriz(j) Then
+                                    encontrado = True
+                                End If
+                            End If
+                        Next
+                        If encontrado And contador = 0 Then
+                            Console.Write("*" & matriz(j) & " ")
+                            contador += 1
+                        Else
+                            Console.Write(matriz(j) & " ")
+                        End If
+
+                    Next
+                    Console.WriteLine()
+
+
             End Select
             Console.ReadLine()
+
+
+
+
+
             Console.Clear()
         Loop While ejercicio < 20
         Console.ReadLine()
