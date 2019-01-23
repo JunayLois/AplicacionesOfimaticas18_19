6cdc6125b35bfe14584f47a8b79f595db99550c0
RamonRS <celmon1@gmail.com>
2019-01-23 09:51:35 +0100

    Ejercicio DNIS

---
+++ Tema 8/EjercicioFicherosDNIS/Funciones.vb
@@ -0,0 +1,124 @@
+﻿Imports System.IO
+Module Funciones
+
+    Sub Escribir(ByVal mensaje As String)
+        Console.WriteLine(mensaje)
+    End Sub
+    Sub MostrarMenu()
+        Escribir("Escoja una opción:")
+        Escribir("a.Introducir nuevo dni")
+        Escribir("b.Buscar DNI")
+        Escribir("c.Mostrar DNIS")
+        Escribir("d.Eliminar DNI")
+    End Sub
+
+    Sub LeerOpcion(ByRef opcion As Char)
+        MostrarMenu()
+        opcion = Convert.ToChar(Console.ReadLine())
+    End Sub
+
+    Sub EscribirFichero(ByVal registro As String)
+        Dim escritor As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(ruta, True)
+        escritor.WriteLine(registro)
+        escritor.Close()
+
+
+    End Sub
+    Sub EscribirFichero(ByVal ruta As String, ByVal registro As String)
+        Dim escritor As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(ruta, True)
+        escritor.WriteLine(registro)
+        escritor.Close()
+
+
+    End Sub
+
+    Sub SobreEscribirFichero(ByVal matriz() As String)
+        Dim escritor As StreamWriter = New StreamWriter(ruta, False)
+
+        For Each registro As String In matriz
+            If Not registro Is Nothing Then
+                escritor.WriteLine(registro)
+            End If
+        Next
+
+        For i As Integer = 0 To matriz.Length - 1 Step 1
+            If Not matriz(i) Is Nothing Then
+                escritor.WriteLine(matriz(i))
+            End If
+        Next
+
+        escritor.Close()
+    End Sub
+
+
+    Function VolcarRegistrosAMatriz() As String()
+        Dim m(99) As String
+        Dim lector As StreamReader = My.Computer.FileSystem.OpenTextFileReader(ruta)
+        Dim i As Integer = 0
+        While Not lector.EndOfStream
+            m(i) = lector.ReadLine()
+            i += 1
+        End While
+        lector.Close()
+        Return m
+    End Function
+
+    Function BuscarRegistro(ByVal reg As String) As Boolean
+        Dim encontrado As Boolean = False
+        Dim lector As StreamReader = My.Computer.FileSystem.OpenTextFileReader(ruta)
+
+        While Not lector.EndOfStream
+            If reg = lector.ReadLine() Then
+                encontrado = True
+                Exit While
+            End If
+        End While
+        lector.Close()
+        Return encontrado
+    End Function
+
+    Function BuscarRegistro(ByVal reg As String, ByRef m() As String) As Boolean
+        Dim encontrado As Boolean = False
+        For Each registro As String In m
+            If registro = reg Then
+                encontrado = True
+                Exit For
+            End If
+        Next
+
+
+        Return encontrado
+    End Function
+
+    Function GetPosicion(ByRef m() As String, ByRef valor As String) As Integer
+        Dim posicion As Integer = -1
+
+        For i As Integer = 0 To m.Length - 1 Step 1
+            If m(i) = valor Then
+                posicion = i
+                Exit For
+            End If
+
+        Next
+        Return posicion
+    End Function
+
+    Function EliminarValor(ByRef m() As String, ByRef valor As String) As Boolean
+        Dim posicion As Integer = GetPosicion(m, valor)
+        Dim eliminado As Boolean = False
+        If posicion > -1 Then
+            m(posicion) = Nothing
+            eliminado = True
+        End If
+        Return eliminado
+    End Function
+
+
+
+
+
+
+
+
+
+End Module
---
+++ Tema 8/EjercicioFicherosDNIS/Module1.vb
@@ -0,0 +1,78 @@
+﻿Option Strict On
+Imports System.IO
+Module Module1
+    Public ruta As String = Directory.GetCurrentDirectory() + "\dnis.txt"
+    Sub Main()
+
+        Dim opcion As Char
+        Do
+
+            'Presentación menú
+            LeerOpcion(opcion)
+
+            'Evaluar opcion
+            Select Case opcion
+                Case "a"c
+                    Dim dniNuevo As String
+                    Escribir("Escribe un nuevo dni")
+                    dniNuevo = Console.ReadLine()
+
+                    EscribirFichero(dniNuevo)
+
+                Case "b"c
+                    'Buscar registro registros
+                    'volcar a matriz los registros
+                    Dim registros() As String = VolcarRegistrosAMatriz()
+
+                    Dim dniABuscar As String
+                    Escribir("Introduce un dni")
+                    dniABuscar = Console.ReadLine()
+                    'Busco en la matriz
+                    If BuscarRegistro(dniABuscar, registros) Then
+                        Console.WriteLine("Está ya almacenado")
+                    Else
+                        Console.WriteLine("No está en los registros")
+                    End If
+                Case "c"c
+                    'Mostrar todos los registros
+
+                    'Dos formas de mostrar registros fichero
+                    '1. Conectado
+                    'Dim lector As StreamReader = New StreamReader(ruta)
+                    'My.Computer.FileSystem.OpenTextFileReader(ruta)
+                    'While Not lector.EndOfStream
+                    '    Dim cadena As String = lector.ReadLine()
+                    '    If Not cadena Is Nothing Then
+                    '        Escribir(cadena)
+                    '    End If
+                    'End While
+                    'lector.Close()
+
+                    '2. Desconectado
+                    Dim actual() As String = VolcarRegistrosAMatriz()
+
+                    For Each registro As String In actual
+                        If Not registro Is Nothing Then
+                            Escribir(registro)
+                        End If
+
+                    Next
+                Case "d"c
+                    Dim actual() As String = VolcarRegistrosAMatriz()
+                    Dim dniEliminar As String
+                    Escribir("Introduce el dni que te quieres cargar")
+                    dniEliminar = Console.ReadLine()
+                    EliminarValor(actual, dniEliminar)
+
+                    'Debo actualizar el fichero con este cambio
+                    SobreEscribirFichero(actual)
+                Case "e"c
+                Case "f"c
+
+
+            End Select
+
+        Loop While opcion <> "f"c
+    End Sub
+
+End Module
